1f1ce98cefd358b5910d691d0a2509a9a0d43f59
yeshixin <ysxdyx@gmail.com>
2021-09-01 17:32:14 +0800

    feature:备案查询接口新增user字段

--- src/service/record-service.js
+++ src/service/record-service.js
@@ -10,7 +10,14 @@ class recordService {
   }
   // 获取备案列表
   async getRecord(id) {
-    let statement = 'SELECT * FROM record' +( id ? ' WHERE id = ?;' : ';');
+    let statement = `
+    SELECT
+    r.id id, r.content content, r.createAt createTime, r.updateAt updateTime,
+    JSON_OBJECT('id', u.id, 'name', u.name) user
+     FROM record r
+    LEFT JOIN users u ON r.user_id = u.id
+    `
+    statement = statement +( id ? ' WHERE r.id = ?;' : ';');
     const result = await connection.execute(statement, id ? [id] : []);
     return result[0];
   }
